9d4a110408cbc40e4653a3f011fec598cdcbaf6f
Daniela Omena <rdaniglad@hotmail.com>
2019-08-14 18:03:19 -0400

    1.

---
+++ order/Controllers/IrolesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace order.Controllers
+{
+    public interface IrolesController
+    {
+        ActionResult Create();
+        ActionResult Create([Bind(Include = "r_id,r_name,r_status")] role role);
+        ActionResult Delete(int? id);
+        ActionResult DeleteConfirmed(int id);
+        ActionResult Details(int? id);
+        ActionResult Edit(int? id);
+        ActionResult Edit([Bind(Include = "r_id,r_name,r_status")] role role);
+        ActionResult Index();
+    }
+}
